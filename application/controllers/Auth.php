ae3d8de964ae488dd4b1d6e28bde48116840ec85
unknown <yodra.muhamad@gmail.com>
2020-07-07 16:05:43 +0700

    fitur edit profile

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -11,6 +11,9 @@ class Auth extends CI_Controller
 
     public function index()
     {
+        if ($this->session->userdata('email')) {
+            redirect('index.php/user');
+        }
         $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
         $this->form_validation->set_rules('password', 'Password', 'required|trim');
 
@@ -61,6 +64,9 @@ class Auth extends CI_Controller
 
     public function registration()
     {
+        if ($this->session->userdata('email')) {
+            redirect('index.php/user');
+        }
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
         $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]');
         $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]');
--- application/controllers/User.php
+++ application/controllers/User.php
@@ -20,4 +20,16 @@ class User extends CI_Controller
 		$this->load->view('user/index', $data);
 		$this->load->view('templates/footer');
 	}
+
+	public function edit()
+	{
+		$data['title'] = 'Edit Profile';
+		$data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+		$this->load->view('templates/header', $data);
+		$this->load->view('templates/sidebar', $data);
+		$this->load->view('templates/topbar', $data);
+		$this->load->view('user/edit', $data);
+		$this->load->view('templates/footer');
+	}
 }
